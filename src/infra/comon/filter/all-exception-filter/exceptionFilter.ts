990f85caa47ad1ae2431b8e8bd2f6697b8cb541c
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-23 22:15:02 -0400

    Created the AllExceptionsFilter

---
+++ src/infra/comon/filter/all-exception-filter/exceptionFilter.ts
@@ -0,0 +1,82 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { AlreadyExistExeption } from 'src/application/exceptions/alreadyExist';
+import { ApplicationLogger } from 'src/application/logger/logger';
+import { InvalidCreateEntityArgumentExeption } from 'src/domain/exceptions/InvalidCreateEntityARgument';
+import { NotFoundEntityExeption } from 'src/domain/exceptions/NotFoundEntity';
+
+type ExceptionData = {
+  statusCode: HttpStatus;
+  message: string;
+};
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    private readonly logger: ApplicationLogger,
+  ) {}
+
+  catch(exception: any, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const resquest = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    const { statusCode, message } = this.mapExceptionDataByInstance(exception)
+      .forExpection(InvalidCreateEntityArgumentExeption, HttpStatus.BAD_REQUEST)
+      .forExpection(NotFoundEntityExeption, HttpStatus.NOT_FOUND)
+      .forExpection(AlreadyExistExeption, HttpStatus.CONFLICT).exceptionData;
+
+    const responseData = {
+      statusCode,
+      timestamp: new Date().toISOString(),
+      path: resquest.url,
+      message,
+    };
+
+    this.logMessage(resquest, statusCode, exception);
+    httpAdapter.reply(response, responseData, statusCode);
+  }
+
+  private mapExceptionDataByInstance(exception: any, data?: ExceptionData) {
+    let exceptionData = data ?? {
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal server error',
+    };
+
+    const forExpection = (exptionClass: any, statusCode: HttpStatus) => {
+      if (exception instanceof exptionClass) {
+        exceptionData = { statusCode, message: exception.message };
+      }
+
+      return this.mapExceptionDataByInstance(exception, exceptionData);
+    };
+
+    return {
+      forExpection,
+      exceptionData,
+    };
+  }
+
+  private logMessage(request: any, statusCode: HttpStatus, exception: any) {
+    if (statusCode === HttpStatus.INTERNAL_SERVER_ERROR) {
+      this.logger.error(
+        `End request for ${request.path}`,
+        `method = ${request.method} status = ${statusCode} message = ${exception.message}`,
+      );
+
+      return;
+    }
+
+    this.logger.warn(
+      `End request for ${request.path}`,
+      `method = ${request.method} status = ${statusCode} message = ${exception.message}`,
+    );
+  }
+}
--- src/infra/use-cases-proxy/use-cases-proxy.module.ts
+++ src/infra/use-cases-proxy/use-cases-proxy.module.ts
@@ -2,12 +2,14 @@ import { Module, DynamicModule } from '@nestjs/common';
 import { CreateUserUseCase } from 'src/application/useCases/CreateUser';
 import { BcryptHasherModule } from '../adapters/bcrypt-hasher/bcrypt-hasher.module';
 import { BcryptHasherService } from '../adapters/bcrypt-hasher/bcrypt-hasher.service';
+import { LoggerModule } from '../logger/logger.module';
+import { LoggerService } from '../logger/logger.service';
 import { RepositoriesModule } from '../repositories/repositories.module';
 import { TypeormUserRepository } from '../repositories/typeorm-user-repository/typeorm-user-repository';
 import { UseCaseProxy } from './useCasesProxy';
 
 @Module({
-  imports: [RepositoriesModule, BcryptHasherModule],
+  imports: [LoggerModule, RepositoriesModule, BcryptHasherModule],
 })
 export class UseCasesProxyModule {
   static CREATE_USER_USECASE_PROXY = 'CreateUserUseCaseProxy';
@@ -17,12 +19,16 @@ export class UseCasesProxyModule {
       module: UseCasesProxyModule,
       providers: [
         {
-          inject: [TypeormUserRepository, BcryptHasherService],
+          inject: [LoggerService, TypeormUserRepository, BcryptHasherService],
           provide: UseCasesProxyModule.CREATE_USER_USECASE_PROXY,
           useFactory: (
+            logger: LoggerService,
             userRepository: TypeormUserRepository,
             hasher: BcryptHasherService,
-          ) => new UseCaseProxy(new CreateUserUseCase(userRepository, hasher)),
+          ) =>
+            new UseCaseProxy(
+              new CreateUserUseCase(userRepository, hasher, logger),
+            ),
         },
       ],
       exports: [UseCasesProxyModule.CREATE_USER_USECASE_PROXY],
