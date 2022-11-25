0860de20f54be7728f0e9fb7a902822da02aebd4
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-24 21:32:05 -0400

    Created the MoviesReposiotry interface

---
+++ src/domain/repositories/MovieRepositry.ts
@@ -0,0 +1,6 @@
+import { Repository } from '../core/Repository';
+import { Movie } from '../entities/Movie';
+
+export interface MoviesReposiotry extends Repository<Movie> {
+  getByTitle(title: string): Promise<Movie | undefined>;
+}
