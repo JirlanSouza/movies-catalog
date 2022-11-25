4c1cdce6c59f462de202e0b0e24cefe5509ba3d0
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-24 21:32:28 -0400

    Created the CreateMovieUseCase class

---
+++ src/application/useCases/movie/CreateMovie.ts
@@ -0,0 +1,30 @@
+import { AlreadyExistExeption } from 'src/application/exceptions/alreadyExist';
+import { ApplicationLogger } from 'src/application/logger/logger';
+import { Movie } from 'src/domain/entities/Movie';
+import { MoviesReposiotry } from 'src/domain/repositories/MovieRepositry';
+import { CreateMovieDto } from './CreateMovieDto';
+
+export class CreateMovieUseCase {
+  constructor(
+    private readonly moviesRepository: MoviesReposiotry,
+    private readonly logger: ApplicationLogger,
+  ) {}
+
+  async execute(data: CreateMovieDto): Promise<Movie> {
+    const existMovie = await this.moviesRepository.getByTitle(data.title);
+
+    if (existMovie) {
+      throw new AlreadyExistExeption('Movie already exist');
+    }
+
+    const movieReleaseDate = new Date(data.releseDate);
+
+    const movie = new Movie({ ...data, releseDate: movieReleaseDate });
+    await this.moviesRepository.save(movie);
+    this.logger.log(
+      'CreateMovieUseCase execute',
+      'The new movie has been created',
+    );
+    return movie;
+  }
+}
---
+++ src/application/useCases/movie/CreateMovieDto.ts
@@ -0,0 +1,10 @@
+export interface CreateMovieDto {
+  title: string;
+  genre: string;
+  overview: string;
+  company: string;
+  releseDate: string;
+  votesAvg: number;
+  votesCount: number;
+  runtimeUrl: string;
+}
